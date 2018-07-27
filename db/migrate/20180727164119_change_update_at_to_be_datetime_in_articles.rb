2c9eb7c8ac664224779be5497da01b075176cacc
macb <macb88@gmail.com>
2018-07-27 17:34:51 +0000

    Modify db migration. Column updated_at with correct type

--- db/migrate/20180727164119_change_update_at_to_be_datetime_in_articles.rb
+++
@@ -1,5 +0,0 @@
-class ChangeUpdateAtToBeDatetimeInArticles < ActiveRecord::Migration[5.2]
-  def change
-    change_column :articles, :updated_at, :datetime
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_27_164119) do
+ActiveRecord::Schema.define(version: 2018_07_23_154525) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
